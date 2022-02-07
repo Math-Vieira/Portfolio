558f99e2ac0fdca60e1526338dd42f32780f6aa2
Matheus <you@example.com>
2022-02-07 14:36:38 -0300

    adicionando botao ver mais

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 const meuNome = document.querySelector('.meu-nome');
+const botaoVerMais = document.querySelector('button');
+const habilidades = document.querySelectorAll('.escondido');
+
+
+function cliqueVer (e) {
+    habilidades.forEach( (elemento) => {
+        if (elemento.classList.contains('escondido')) {
+            elemento.classList.remove('escondido');
+            elemento.classList.add('visivel');
+            botaoVerMais.innerHTML = 'Ver menos';
+        } else {
+            elemento.classList.add('escondido');
+            elemento.classList.remove('visivel');
+            botaoVerMais.innerHTML = 'Ver mais';
+        }
+    } )
+}
+
+function maisHabilidades (elemento) {
+    elemento.addEventListener('click', cliqueVer)
+}
 
 function typeWriter (nome) {
     const meuNome = nome.innerHTML.split('');
@@ -12,3 +33,5 @@ function typeWriter (nome) {
 }
 
 typeWriter(meuNome);
+maisHabilidades(botaoVerMais);
+
