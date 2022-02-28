3b5c7c39e7e48ff675fab7e1120a020ba372ccb7
Matheus <you@example.com>
2022-02-28 01:01:55 -0300

    teste 4

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,5 +1,5 @@
 window.sr = ScrollReveal({ reset:true});
-ScrollReveal({ distance: '150px' });
+/* ScrollReveal({ distance: '150px' }); */
 sr.reveal('.descricao', {duration : 3000, origin: 'left'})
 sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
 sr.reveal('.desenho-bg', {duration : 3000,  origin: 'left'})
