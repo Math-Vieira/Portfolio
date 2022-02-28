8a78168f8494f1f67b4bb18ff87cc5cb9744852d
Matheus <you@example.com>
2022-02-28 00:50:48 -0300

    testando erro

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,3 @@
 window.sr = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '150px' });
 sr.reveal('.desenho', {duration : 3000, origin:'right'})
-sr.reveal('.descricao', {duration : 3000, origin: 'left'})
-sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
-sr.reveal('.avatar', {duration : 3000,  origin: 'left'})
-sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
-sr.reveal('.projetos-itens', {duration : 3000, origin:'bottom'})
-sr.reveal('.project', {duration : 3000, origin:'bottom'})
-sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
-sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
-sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
