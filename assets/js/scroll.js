d0928c3cd2472c9d0477f72a39cebc692cc5bcb6
Matheus <you@example.com>
2022-02-28 01:06:25 -0300

    problema resolvido

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,11 @@
 window.sr = ScrollReveal({ reset:true});
-ScrollReveal({ distance:'50px' });
+ScrollReveal({ distance: '50px' });
 sr.reveal('.descricao', {duration : 3000, origin: 'left'})
 sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
 sr.reveal('.desenho-bg', {duration : 3000,  origin: 'left'})
 sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
+sr.reveal('.projetos-itens', {duration : 3000, origin:'bottom'})
+sr.reveal('.project', {duration : 3000, origin:'bottom'})
+sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
+sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
+sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
