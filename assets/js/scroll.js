f91bff76263b9fda6f1ea37a8cc7a96aad4b1e37
Matheus Vieira <92286969+Math-Vieira@users.noreply.github.com>
2023-08-28 05:20:41 -0300

    Update scroll test

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -10,3 +10,20 @@ sr.reveal('.project', {duration : 3000, origin:'bottom'})
 sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
 sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
 sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+
+// Função para verificar se as ferramentas de desenvolvimento estão abertas
+function detectarFerramentasDeDesenvolvimento() {
+    const devtools = /./;
+    devtools.toString = function() {
+        // Esta mensagem será exibida nas ferramentas de desenvolvimento
+        alert('Não é permitido abrir as ferramentas de desenvolvimento.');
+        // Redirecionar o usuário para o Google
+        window.location.href = 'https://www.google.com';
+    };
+
+    // Imprimir o objeto devtools no console
+    console.log(devtools);
+}
+
+// Chamar a função de detecção
+detectarFerramentasDeDesenvolvimento();
