ed0c897a1800967441a9f733853b8afcc6d96569
Matheus Vieira <92286969+Math-Vieira@users.noreply.github.com>
2023-08-28 05:26:19 -0300

    Update scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -11,15 +11,3 @@ sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
 sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
 sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
 
-let widthThreshold = window.outerWidth - window.innerWidth;
-
-function detectarFerramentasDeDesenvolvimento() {
-    if (window.outerWidth - window.innerWidth > widthThreshold) {
-        console.log("Ferramentas de desenvolvimento abertas");
-        // Aqui você pode chamar a função que deseja executar
-    } else {
-        alert('tá de boa')
-    }
-}
-
-setInterval(detectarFerramentasDeDesenvolvimento, 1000);
