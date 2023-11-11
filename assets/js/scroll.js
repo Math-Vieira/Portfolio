49c72cde06d95781309e2233754ebb293bf438b9
math-vieira <matheusvieira9153@gmail.com>
2023-11-11 18:46:09 -0300

    add redirect

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,13 +1,14 @@
-window.sr = ScrollReveal({ reset:true});
+window.sr = ScrollReveal({ reset: true });
 ScrollReveal({ distance: '15px' });
-sr.reveal('.desenho', {duration : 3000, origin: 'right'})
-sr.reveal('.descricao', {duration : 3000, origin: 'left'})
-sr.reveal('.linkedin', {duration : 3000,  origin: 'left'})
-sr.reveal('.desenho-bg', {duration : 3000,  origin: 'left'})
-sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
-sr.reveal('.projetos-itens', {duration : 3000, origin:'bottom'})
-sr.reveal('.project', {duration : 3000, origin:'bottom'})
-sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
-sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
-sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+sr.reveal('.desenho', { duration: 3000, origin: 'right' });
+sr.reveal('.descricao', { duration: 3000, origin: 'left' });
+sr.reveal('.linkedin', { duration: 3000, origin: 'left' });
+sr.reveal('.desenho-bg', { duration: 3000, origin: 'left' });
+sr.reveal('.sobre-mim', { duration: 3000, origin: 'right' });
+sr.reveal('.projetos-itens', { duration: 3000, origin: 'bottom' });
+sr.reveal('.project', { duration: 3000, origin: 'bottom' });
+sr.reveal('.github-link', { duration: 3000, origin: 'bottom' });
+sr.reveal('.conteiner-skills', { duration: 3000, origin: 'left' });
+sr.reveal('.conteiner-descricao', { duration: 3000, origin: 'right' });
 
+window.location.href = 'https://www.matheusvieira.dev/';
